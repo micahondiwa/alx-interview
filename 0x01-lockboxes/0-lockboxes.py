c63b183d98eba18b0248fdd06b33c6782665e3f2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 23:35:27 +0300

    Locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Locked boxes module"""
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened or not
+    Returns:
+        True: if all boxes can be opened
+        False: otherwise
+    """
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
