ad07a571222896f03863f13aeef8ebc86963e6a1
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-19 06:00:02 +0300

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const API_URL = 'https://swapi-api.alx-tools.com/api/';
 
 if (process.argv.length > 2) {
     request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
