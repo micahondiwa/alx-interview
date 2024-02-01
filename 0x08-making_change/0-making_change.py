81d72b5b280879f77421ba73cf32190fa4e88086
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-02-01 17:43:32 +0300

    Fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,4 +3,21 @@
 
 
 def makeChange(coins, total):
-    return 0
+    """Returns the minimum number of 
+    coins needed to get the total,"""
+    if total <= 0:
+        return 0
+    rem = total
+    coins_count = 0
+    coins_idx = 0
+    sorted_conins = sorted(coins, reverse = True)
+    n = len(coins)
+    while rem > 0:
+        if coins_idx >= n:
+            return -1
+        if rem - sorted_conins[coins_idx] >= 0:
+            rem -= sorted_conins[coins_idx]
+            coins_count += 1
+        else:
+            coins_idx += 1
+    return coins_count
